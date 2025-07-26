6ac6d71024e68d23d706c386f833c8a4d8c32e92
Miss shelby <ceeyntia@gmail.com>
2025-07-27 00:19:20 +0100

    updated text size in featurs card

--- src/features/pages/Home/components/Features.jsx
+++ src/features/pages/Home/components/Features.jsx
@@ -11,7 +11,7 @@ const Features = () => {
             <div className="xl:max-w-lg w-full pb-6 xl:pb-0">
               <p className="text-sm sm:text-base text-[#616161] text-center sm:text-left dark:text-[#EDEDED] font-sans ">CMS Compatibility – Built for the Tools You Know</p>
                <p className="text-xl lg:text-3xl font-bold text-center sm:text-left font-sans text-[#130D08] dark:text-[#fff] pt-2">Works Perfectly with WordPress, Joomla, and More</p>
-                <p className="text-xl xl:max-w-3xl text-center sm:text-left mx-auto font-sans font-medium text-[#616161] dark:text-[#EDEDED] pt-2 ">WeHostify is designed to run seamlessly with the world’s most trusted content management systems — so you can keep building with what you’re already comfortable using.  </p>
+                <p className="text-sm xl:max-w-3xl text-center sm:text-left mx-auto font-sans font-medium text-[#616161] dark:text-[#EDEDED] pt-2 ">WeHostify is designed to run seamlessly with the world’s most trusted content management systems — so you can keep building with what you’re already comfortable using.  </p>
             </div>
              <img className=' object-contain lg:hidden absolute z-[0] right-0 top-28 opacity-40' src={BGGRADIENT} alt='Yellow gradient'/>
             <div className="grid  sm:px-0 lg:grid-cols-2 grid-rows-2 gap-6 z-10 ">
